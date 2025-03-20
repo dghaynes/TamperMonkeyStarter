244c4d5a52c222cd7d2949fa6c1193ec2cafbce1
dghaynes <dghaynnes@proton.me>
2025-03-20 11:37:43 -0700

    first commit

---
+++ Boilerplate.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Boilerplate
+// @version      2025-03-20
+// @description  Starter code ...
+// @author       @dghaynes
+// @grant        none
+// @include      *localhost*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const h1 = document.querySelector('h1');
+    h1.textContent = 'Goodnight world!';
+
+})();
