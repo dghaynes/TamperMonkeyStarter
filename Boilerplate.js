b82ae3e538b2949a3e0283eed95d60112e64bbd6
dghaynes <dghaynnes@proton.me>
2025-03-20 11:46:26 -0700

    adding download, and update url

--- Boilerplate.js
+++ Boilerplate.js
@@ -1,5 +1,8 @@
 // ==UserScript==
 // @name         Boilerplate
+// @description    A script to install all browser scripts for TOC
+// @downloadURL    https://github.com/dghaynes/TamperMonkeyStarter/blob/main/Boilerplate.js?raw=1
+// @updateURL      https://github.com/dghaynes/TamperMonkeyStarter/blob/main/Boilerplate.js?raw=1
 // @version      2025-03-20
 // @description  Starter code ...
 // @author       @dghaynes
