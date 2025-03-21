dd825bb3703865eeb27b587b29ac1a137d568d2a
dghaynes <dghaynnes@proton.me>
2025-03-21 09:50:02 -0700

    version 1

--- Boilerplate.js
+++ Boilerplate.js
@@ -3,14 +3,17 @@
 // @description    A script to install all browser scripts for TOC
 // @downloadURL    https://github.com/dghaynes/TamperMonkeyStarter/blob/main/Boilerplate.js?raw=1
 // @updateURL      https://github.com/dghaynes/TamperMonkeyStarter/blob/main/Boilerplate.js?raw=1
-// @version      2025-03-20
+// @version      0.1.00
 // @description  Starter code ...
 // @author       @dghaynes
-// @grant        none
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
 // @include      *localhost*
 // ==/UserScript==
 
 (function() {
+
     'use strict';
 
     // Your code here...
